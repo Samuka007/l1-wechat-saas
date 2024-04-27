8691330b08ddd5a52ecaa16a61303cdf2f3b05a7
Samuka007 <samuka007@qq.com>
2024-04-27 11:27:16 +0800

    finish initial config

--- config/dev.py
+++ config/dev.py
@@ -45,13 +45,13 @@ DEBUG = True
 
 # 本地开发数据库设置
 # USE FOLLOWING SQL TO CREATE THE DATABASE NAMED APP_CODE
-# SQL: CREATE DATABASE `framework_py` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; # noqa: E501
+# SQL: CREATE DATABASE `bk_demo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci; # noqa: E501
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": APP_CODE,  # noqa
-        "USER": "root",
-        "PASSWORD": "",
+        "USER": "mysql",
+        "PASSWORD": "mysql",
         "HOST": "localhost",
         "PORT": "3306",
     },
